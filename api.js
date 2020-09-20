af9893ea724008c57fd1c9f49372a877e41833b3
wildpow <element1028@gmail.com>
2020-09-19 23:57:42 -0700

    feat: first aws function

---
+++ api.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const Api = require("claudia-api-builder");
+const api = new Api();
+
+api.get("pizzas", () => {
+  return ["Capricciosa", "Quattro Formaggi", "Napoletana", "Margherita"];
+});
+
+module.exports = api;
